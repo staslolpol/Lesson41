21faf19be3f3d9d6c68d93c8a99e3ef786022bd0
staslolpol <staspriho9@gmail.com>
2026-02-16 18:26:16 +0300

    2

--- Project1/TEST01.cpp
+++ Project1/TEST01.cpp
@@ -1,33 +1,33 @@
-#include <iostream>
-
-#define DEFAULT_SIZE 10 // C-NOTATION
-
-using namespace std;
-int main() {
-	//int numbers[10];
-	//int vector[10];
-
-	// 1)
-	//int array[10];
-
-	//int length = 10;
-	const int SIZE = 10;  // C++ notation
-	int array[SIZE];
-
-	int index = 0;
-
-	for (int index = 0; index < SIZE; index++)
-	{
-		array[index] = 0;	//0
-
-	}
-
-	for (int index = 0; index < SIZE; index++)
-	{
-		cout << '[' << index + 1 << "] --> " << array[index] << endl;	//0
-
-	}
-
-	return 0;
-}
-
+//#include <iostream>
+//
+//#define DEFAULT_SIZE 10 // C-NOTATION
+//
+//using namespace std;
+//int main() {
+//	//int numbers[10];
+//	//int vector[10];
+//
+//	// 1)
+//	//int array[10];
+//
+//	//int length = 10;
+//	const int SIZE = 10;  // C++ notation
+//	int array[SIZE];
+//
+//	int index = 0;
+//
+//	for (int index = 0; index < SIZE; index++)
+//	{
+//		array[index] = 0;	//0
+//
+//	}
+//
+//	for (int index = 0; index < SIZE; index++)
+//	{
+//		cout << '[' << index + 1 << "] --> " << array[index] << endl;	//0
+//
+//	}
+//
+//	return 0;
+//}
+//
--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,31 +1,29 @@
-//#include <iostream>
-//
-//double calculet_average_mark(int a, int b, int c);
-//
-//#define STUDENT_NUMBER 3.0
-//
-//using namespace std;
-//int main() {
-//	int a = 0;
-//	int b = 0;
-//	int c = 0;
-//
-//	cout << "Input first mark: " << endl;
-//	cin >> a;
-//
-//	cout << "Input second mark: " << endl;
-//	cin >> b;
-//
-//	cout << "Input third mark: " << endl;
-//	cin >> c;
-//
-//	double average = calculet_average_mark(a, b, c);
-//
-//	cout << "The average mark: " << average << endl;
-//
-//	return 0;
-//}
-//
-//double calculet_average_mark(int a, int b, int c) {
-//	return (a + b + c) / STUDENT_NUMBER;
-//}
+#include <iostream>
+
+
+#define STUDENT_NUMBER 3
+
+using namespace std;
+int main() {
+	int marks[STUDENT_NUMBER];
+
+
+	cout << "Input students mark: " << endl;
+	for (int index = 0; index < STUDENT_NUMBER; index++)
+	{
+		cin >> marks[index];
+	}
+
+	double avg = 0;
+
+	for (int index = 0; index < STUDENT_NUMBER; index++)
+	{
+		avg += marks[index];
+	}
+
+	cout << "The average mark: " << avg / STUDENT_NUMBER << endl;
+
+	return 0;
+}
+
+
